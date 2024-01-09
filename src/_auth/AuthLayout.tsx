dadb0006a8f49ada4d05049b95d3608c575e9eb6
Aadesh Santosh Bhujbal <aadeshbhujbal1@gmail.com>
2024-01-09 15:52:43 +0530

    authPages_added

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Outlet,Navigate } from 'react-router-dom'
 
 const AuthLayout = () => {
+
+
+  const  isAuthenticated = false;
   return (
-    <div>AuthLayout</div>
+    <>
+    
+    {isAuthenticated?( <Navigate to="/" />):(<><section className='flex flex-1 justify-center items-center flex-col py-4'>
+        <Outlet /></section><img alt='logo' src='/assets/images/side-img.svg' className='hidden xl:block h-screen w-1/2 object-cover bg:no-repeat' /></>
+      )}
+      </>
   )
 }
 
-export default AuthLayout
+export default AuthLayout;
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import * as z from "zod"
+
+export const SignupValidation = z.object({
+    username: z.string().min(2,{message:"Too Short"}).max(50),
+    name: z.string().min(2,{message:"Too Short"}),
+    email:z.string().email(),
+    password:z.string().min(8,{message:'Password Must Be At Least 8 Characters'})
+  })
+  
